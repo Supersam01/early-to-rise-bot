c12051ebd57cbc7149d2b32023b87c6ac5f5df43
Supersam01 <80099053+Supersam01@users.noreply.github.com>
2026-01-17 14:50:28 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,15 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8397097729:AAE7eextZCH1DuqMjyNjxhPy2t1OqxO7HJo")
-ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "@early_to_rise")
+ADMIN_USER_ID = int(os.getenv("ADMIN_USER_ID", "8251843110"))
 
 DB_PATH = os.getenv("DB_PATH", "app/early_to_rise.db")
 
-DELIVERY_TIME_WINDOW_MINUTES = 10
+PACKAGING_FEE_PER_ITEM = 200
+DELIVERY_WINDOW_MINUTES = 10
+
+HOSTEL_PRIORITY = [
+    "Dorcas", "Deborah", "Lydia", "Mary",
+    "Daniel", "Joseph", "Paul", "Peter",
+    "Esther", "John"
+]
