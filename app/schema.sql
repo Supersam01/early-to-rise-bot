7b1a91e124fcae8b7d5c8d899514907fc46e93e7
Supersam01 <80099053+Supersam01@users.noreply.github.com>
2026-01-17 18:06:43 +0100

    Update schema.sql

--- app/schema.sql
+++ app/schema.sql
@@ -1,1 +1,18 @@
+CREATE TABLE IF NOT EXISTS orders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    ref_code TEXT UNIQUE,
+    hostel TEXT,
+    items TEXT, -- JSON string
+    total_price INTEGER,
+    status TEXT DEFAULT 'PENDING',
+    delivery_slot TEXT DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS stock (
+    item_name TEXT PRIMARY KEY,
+    quantity INTEGER,
+    last_reset_date TEXT
+);
 
