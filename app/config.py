334b3f62b7a3263fc4bc0c955b989359d32809e8
Supersam01 <80099053+Supersam01@users.noreply.github.com>
2026-01-17 14:46:48 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,23 +1,8 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8397097729:AAE7eextZCH1DuqMjyNjxhPy2t1OqxO7HJo")
-DB_PATH = os.getenv("DB_PATH", "early_to_rise.db")
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "@early_to_rise")
 
-ADMIN_ID = 8251843110
+DB_PATH = os.getenv("DB_PATH", "app/early_to_rise.db")
 
-HOSTEL_ORDER = [
-    "Dorcas",
-    "Deborah",
-    "Lydia",
-    "Mary",
-    "Daniel",
-    "Joseph",
-    "Paul",
-    "Peter",
-    "Esther",
-    "John"
-]
-
-# time slots rule
-TIME_SLOT_FIRST_15 = ("05:30", "05:40")
-TIME_SLOT_LAST_15 = ("07:00", "07:10")
+DELIVERY_TIME_WINDOW_MINUTES = 10
